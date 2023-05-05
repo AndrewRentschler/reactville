c8fbc532233d54665ac4cc4490a4603c0808864d
Andrew Rentschler <98727929+AndrewRentschler@users.noreply.github.com>
2023-05-04 22:24:37 -0500

    Fix Burger Shop Panel Render

--- src/Components/BurgerShop/BurgerStack.jsx
+++ src/Components/BurgerShop/BurgerStack.jsx
@@ -4,7 +4,10 @@ import Ingredient from "./Ingredient"
 const BurgerStack = (props) => {
   return (
   <>
-  {!props.stack.length ? <h4>Nuthin</h4>:
+  {!props.stack.length ? 
+    <ul className="burger-stack">
+      Nothing on Burger
+    </ul>:
     <ul className="burger-stack">
       {props.stack.map((ingredient,idx) => (
           <Ingredient 
